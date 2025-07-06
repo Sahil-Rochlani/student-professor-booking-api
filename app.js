c4002cf9996e0a02dca99c99ba95f004e2e4cbd7
Sahil Rochlani <106559770+Sahil-Rochlani@users.noreply.github.com>
2025-07-06 18:24:26 +0530

    Added skeletal route files and mounted them in app.js

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
+const professorSlotRouter = require('./routes/professorSlotRoutes')
+const userRouter = require('./routes/userRoutes')
+const studentBookingRouter = require('./routes/studentBookingRoutes')
+
 
 app.use(cookieParser())
 app.use(express.json())
+
+app.use('/user', userRouter)
+app.use('/professor', professorSlotRouter)
+app.use('/student', studentBookingRouter)
 
 module.exports = app
---
+++ routes/userRoutes.js
@@ -0,0 +1,5 @@
+const { Router } = require('express')
+const userRouter = Router()
+require('dotenv').config()
+
+module.exports = userRouter
